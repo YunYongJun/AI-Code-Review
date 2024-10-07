eb188a3cbdb8b88e8f1632255bdcf74634d12db5
sungho <135408197+parksh3070@users.noreply.github.com>
2024-10-07 15:11:38 +0900

    spring-boot
    스프링 부트 개발 환경

---
+++ ai-code-gem/src/test/java/com/aicodegem/ai_code_gem/AiCodeGemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicodegem.ai_code_gem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
