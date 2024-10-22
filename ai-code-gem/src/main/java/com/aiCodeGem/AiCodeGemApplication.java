dc11c9248676a8549210997f1dc986c7cb74f6d3
YunYongJun <a86622014@gmail.com>
2024-10-22 10:01:11 +0900

    스프링 오류

--- ai-code-gem/src/main/java/com/aiCodeGem/AiCodeGemApplication.java
+++ ai-code-gem/src/main/java/com/aiCodeGem/AiCodeGemApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiCodeGemApplication {
-
+public class AIApplication {
     public static void main(String[] args) {
         SpringApplication.run(AiCodeGemApplication.class, args);
     }
