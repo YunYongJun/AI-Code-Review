0bc576b73797aa0b066585f6d555a6f0cadbac52
pkun2 <pkun2@naver.com>
2024-11-25 16:30:43 +0900

    테스트

--- src/main/java/com/aicodegem/controller/AchievementController.java
+++ src/main/java/com/aicodegem/controller/AchievementController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.List; //주석
 
 // Achievement API를 제공하는 컨트롤러 클래스
 @RestController
